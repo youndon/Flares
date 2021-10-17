b3036568a23753c70e3d57c03bcf207e81dd5435
yon <bourntivoli@gmail.com>
2021-10-17 10:16:11 +0100

    21/10/21

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,66 +1,19 @@
-import AtlasLib.Coding
-import AtlasLib.Uncoding
-import java.math.BigDecimal
-import java.math.BigInteger
-import java.math.MathContext
-import java.math.RoundingMode
-import java.util.*
-import kotlin.math.pow
-import kotlin.time.*
+import kotlinx.coroutines.processNextEventInCurrentThread
+import java.util.function.UnaryOperator
 
 fun main() {
 
-
 }
 
-
-/*
-Create a euphonious word.
-    println("player".windowed(3).count {
-   it.all { a -> a in "aeiouy" } xor it.none { a -> a in "aeiouy" } })
-
-
-Cutting out the middle of a string.
-    val s = "ab*cd"
-    if (s.length % 2 == 0) {
-        s.chunked(s.length/2).also { println(it.first().dropLast(1)+it.last().drop(1)) }
-    } else println(s.removeRange(s.lastIndex/2..(s.lastIndex/2)))
-
-
-Guard On The Gate.
-      println(s.plus(ss).joinToString())
-
-Move The First N Character.
-  val s = "Hello 3".split(' ')
-    val n = s.last().last().digitToInt()-1
-    print(s.first().takeLast(n)+s.first().dropLast(n))
-
-Playing with Numbers.
-    val s = "8 11 1 2 3"
-    println(
-        s.split(' ').sumOf { it.toInt() }.toString() + " " +
-                s.removeRange(s.lastIndex-3,s.lastIndex-1)
-    )
-
-
-Playing with time.
-    println("${calendar.day}${calendar.month}${calendar.year}")
-
-A simple calculator.
- val n = "97 ! 50"
-    val first = n.substringBefore(' ').toInt()
-    val last= n.substringAfterLast(' ').toInt()
-    try {
-        println(when  {
-           '+' in n -> first + last
-           '-' in n -> first - last
-           '/' in n -> first / last
-           '*' in n -> first * last
-           else -> "Unknown operator"
-        })
-    } catch (e: Exception) {
-        println("Division by 0!")
+fun solution(products: List<String>, product: String) {
+    products.withIndex().forEach {
+       if (it.value == product) { println(it.index) }
     }
+}
 
-*/
-
+fun solution(strings: MutableList<String>, str: String): MutableList<String> {
+   strings.forEach { if (it == str) {
+       strings[strings.indexOf(it)] = "+++" }
+   }
+    return strings
+}
