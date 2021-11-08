ef8cafe476eafb8cc83da8492c2517a07bd0d4f1
yon <bourntivoli@gmail.com>
2021-11-08 17:32:20 +0100

    08/11/21

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,7 @@
-import java.io.File
-import java.io.IOException
 
 fun main() {
 
-
 }
-
-
 
 // Balance checker
 fun balance() {
@@ -23,25 +18,3 @@ fun balance() {
         }
     }
 }
-
-// The first position of the largest element.
-fun sdfsdfs() {
-    /*Write a program that reads a sequence of numbers of undefined size and prints the largest number and the position of its first
-occurrence. The position starts from 1*/
-
-    val lis = listOf(1,2,3,4,5,6,7,6,7)
-    print("${lis.maxOf { it }} ${lis.count { co -> co == lis.maxOf { it } }}")
-
-}
-
-
-// The try-catch-finally statement Complex handling
-fun suspiciousFunction (param: Int) = param.runCatching {
-    when (this) {
-        0 -> throw Exception("Some exceptions?")
-        1 -> throw ArithmeticException("Division by zero")
-        2 -> throw Exception("An exception occurred here")
-        else -> throw IOException("The IOException occurred")
-    }
-}.onSuccess { println("Handling completed successfully!") }
-
