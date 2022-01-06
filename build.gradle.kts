117ba329185d72df73d517a645fc3c99835b533c
yon <bourntivoli@gmail.com>
2022-01-06 19:06:45 +0100

    06/01/22

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 }
 
 repositories {
-    jcenter()
     mavenCentral()
 }
 
@@ -13,7 +12,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.6.0")
     testImplementation("io.mockk:mockk:1.12.1")
--- src/main/kotlin/Atlasx/scheduling/ExperimentalCoroutineDispatcher.kt
+++ src/main/kotlin/Atlasx/scheduling/ExperimentalCoroutineDispatcher.kt
@@ -1,18 +1,3 @@
 package coroutines.scheduling
 
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.scheduling.ExperimentalCoroutineDispatcher
-
-@InternalCoroutinesApi
-class ExperimentalCoroutineDispatcher {
-    @InternalCoroutinesApi
-    lateinit var experimentalCoroutineDispatcher:ExperimentalCoroutineDispatcher
-
-    init {
-        experimentalCoroutineDispatcher.run {
-            this.blocking()
-            this.limited(0)
-        }
-    }
-
-}
+class ExperimentalCoroutineDispatcher
