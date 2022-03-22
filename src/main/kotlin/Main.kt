c3ac77a606b2b65d9b6cfba7ec7942f60825d426
yon <bourntivoli@gmail.com>
2022-03-22 16:31:40 +0100

    22/03/22

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,5 @@ import kotlinx.coroutines.internal.synchronized
 
 @OptIn(InternalCoroutinesApi::class)
 fun main() {
-    // Removing chords.
-//   .replace(Regex("[ADGC]m? "), "")
-
-    // Web colors.
-//    "#[0-9a-fA-F]{6}\\b".toRegex()
-//        .findAll(
-//            "Web colors are colors used when displaying web pages. They can be represented as #123ABC: three pairs of hexadecimal digits. The first two digits are red, the two in the middle are green and the last two digits are blue. Example: #AABBCC, #ff0000."
-//        ).forEach { println(it.value) }
-
-    //
-
 
 }
