43d8304faea8331fa0d752de699621c3b77d6f0f
yon <bourntivoli@gmail.com>
2022-04-18 02:36:06 +0100

    18/04/22

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,31 @@
 import java.util.Collections
+import kotlin.coroutines.Continuation
 
 fun main() {
+    (90..97).forEach {
+        println("\u001b[0;${it}m Normal Text!")
+    }
+    println("\u001B[0m")
+    (90..97).forEach {
+        println("\u001b[1;${it}m Bold Text!")
+    }
+    println("\u001B[0m")
+    (90..97).forEach {
+        println("\u001b[3;${it}m Italic Text!")
+    }
+    println("\u001B[0m")
+    (90..97).forEach {
+        println("\u001b[4;${it}m UnderLine Text!")
+    }
+    println("\u001B[0m")
+    (90..97).forEach {
+        println("\u001b[9;${it}m MiddleLine Text!")
+    }
+    println("\u001B[0m")
+    (90..97).forEach {
+        println("\u001b[7;${it}m Background Text!")
+    }
+    println("\u001B[0m")
 
 }
+
